65a5f3aac0cf18a1cc00dd4592bfaf9be03519e2
Jorgen <jorgen@cyberb.local>
2021-02-15 08:48:10 +0100

    Lagt til de fungerende filene som brukes i custom TOC

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Loader from './components/loader';
+import useElementHeight from './hooks/useElementHeight';
+import Collections from './components/collections';
+import communication from './communication/communication';
+import CollectionNotFound from './components/notFound';
+
+
+
+function App() {
+  const documentHeight = useElementHeight(document.documentElement);
+  const [collectionsState, setCollectionsState] = React.useState(null);
+  const [isCommunicationInitialized, setIsCommunicationInitialized] = React.useState(false);
+  const [isAuth, setIsAuth] = React.useState(false);
+
+  React.useEffect(() => {
+    communication.init().then((isAuth) => {
+      setIsAuth(isAuth);
+      console.log('Initialized communication1! isAuth:', isAuth);
+
+      if (isAuth) {
+        communication.requestCollectionsData().then((collections) => {
+          console.log('Received collections', collections);
+          setCollectionsState(collections);
+          setIsCommunicationInitialized(true);
+        });
+      } else { 
+        setIsCommunicationInitialized(true);
+      }
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (isCommunicationInitialized) {
+      communication.updateIFrameHeight(documentHeight);
+      console.log('INIT: ', isCommunicationInitialized);
+    }
+  }, [documentHeight, isCommunicationInitialized]);
+
+  if (!isCommunicationInitialized) {
+    console.log('false');
+    console.log('Start Loading');
+    return <Loader />;
+    console.log('Loading');
+  }
+
+  return (
+    <div className="App">
+      { isAuth ? <Collections collections={collectionsState.data} /> : <CollectionNotFound />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/lesson.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import communication from '../communication/communication';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import IconButton from '@material-ui/core/IconButton';
+import LinearProgress from '@material-ui/core/LinearProgress'; 
+import { createStyles, makeStyles, withStyles } from '@material-ui/core/styles'
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import config from '../config.json';
+import { CardHeader, Card, CardActionArea, CardMedia, CardActions } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        wrapper: {
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            position: 'relative',
+            height: '20rem',
+            width: '16rem',
+            textAlign: 'center',
+            cursor: 'pointer'
+        },
+        buttonProgress: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            marginTop: -50,
+            marginLeft: -50
+        },
+        media: {
+            justifyContent: 'center',
+
+        },
+        cardImgCenter: {
+            display: 'flex',
+            justifyContent: 'center',
+            width: '16rem',
+            maxWidth: '100rem'
+        },
+        styleFlex: {
+            display: 'flex',
+            justifyContent: 'center'
+        },
+        lessonTitle: {
+            fontWeight: 'bold',
+            maxWidth: 400,
+            height: 100
+
+        }
+    }),
+);
+function lessonTime(lesson) {
+    function formatDate(ms) {
+        const date = new Date(Number(ms))
+        return `${date.getUTCHours()}h ${date.getMinutes()}m ${date.getSeconds()}s`
+    }
+
+    return (
+        <p>
+            {formatDate(lesson.time)}
+        </p>
+    )
+}
+function LinearProgressWithLabel(props) {
+    return (
+      <Box display="flex" alignItems="center" lineHeight={100}>
+        <Box width="100%" mr={1}>
+          <LinearProgress variant="determinate" {...props} />
+        </Box>
+        <Box minWidth={10}>
+          <Typography variant="body2" color="textSecondary">{`${Math.round(
+            props.value,
+          )}%`}</Typography>
+        </Box>
+      </Box>
+    );
+  }
+
+  const BorderLinearProgress = withStyles((theme) => ({
+    root: {
+      height: 15,
+      borderRadius: 5,
+      lineHeight: 100,
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: '#00b300',
+    },
+
+  }))(LinearProgressWithLabel);
+
+function Lesson(props) {
+    console.log({props});
+    const classes = useStyles();
+    const onClick = React.useCallback(() => {
+        communication.requestOpenLesson(props.lesson.id);
+    }, [props.lesson]);
+
+    return <React.Fragment>
+        <Card className={classes.wrapper} onClick={onClick}>
+            <CardHeader className={classes.lessonTitle}
+            subheader={
+                <Typography gutterBottom variant="inherit" variantMapping="h2" align='center'>
+                   {props.lesson.name}
+                </Typography>}
+            ></CardHeader>
+            <div className={classes.cardImgCenter}>
+                <CardMedia
+                    className={classes.media}
+                    style={{ width: 130, height: 100, alignContent: 'center' }}
+                    image={`${config['APPLICATION_BASE_URL']}${props.lesson.icon}`}
+                /></div>
+            <CardActions className={classes.wrapper}>
+                <div className={classes.styleFlex}>
+                    <ScheduleIcon>
+                        <ScheduleIcon />
+                    </ScheduleIcon>
+                    <Typography variant="subtitle2" gutterBottom style={{marginLeft: 5, lineHeight: 0}} >{lessonTime(props.lesson)}</Typography>
+                </div>
+                <BorderLinearProgress variant='determinate' value={props.lesson.score}/>
+                
+
+            </CardActions>
+
+
+        </Card>
+    </React.Fragment>
+}
+
+
+export default Lesson;
