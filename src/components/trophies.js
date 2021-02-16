3249913fdf4906b38cf096955eadb1f836fa7a76
Eirik Ødegaard <eirik@kunnskap.no>
2021-02-16 13:05:56 +0100

    mer totalt

--- src/components/trophies.js
+++ src/components/trophies.js
@@ -4,7 +4,8 @@ function Trophies({cmdr}) {
     let tGold = 0;
     let tSilver = 0;
     let tBronze = 0;
-    let scr = 0;
+    let totTime = 0;
+    let totErr = 0;
     console.log("----------");
     console.log({cmdr});
 
@@ -20,16 +21,28 @@ function Trophies({cmdr}) {
                 if(indy.score >= 10 && indy.score < 50){
                     tBronze++;
                 }
+                totErr += indy.errors;
+                //console.log(formatDate(indy.time));
+                //console.log(totTime + " + " + indy.time);
+                totTime = totTime + parseInt(indy.time);
+                //console.log("Nytid: " + totTime);
             }
                 )
         )
-    
+        function formatDate(ms) {
+            const date = new Date(Number(ms))
+            return `${date.getUTCHours()}h ${date.getMinutes()}m ${date.getSeconds()}s`
+        }
 
     console.log(tGold + " " + tSilver + " " + tBronze);
+    console.log(totErr);
+    console.log(formatDate(totTime));
 
     return (
     <React.Fragment>
     <p>Gull: {tGold} Sølv: {tSilver} Bronse: {tBronze}</p>
+    <p>Total feil: {totErr}</p>
+    <p>Total tid: {formatDate(totTime)}</p>
     </React.Fragment>);
 }
 
