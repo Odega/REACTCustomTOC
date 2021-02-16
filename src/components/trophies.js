29d81c2f7f9ec84013c1a941158ed86c065bc4da
Eirik Ødegaard <eirik@kunnskap.no>
2021-02-16 12:23:57 +0100

    trophies4

--- src/components/trophies.js
+++ src/components/trophies.js
@@ -1,38 +1,36 @@
 import React from 'react';
 
 function Trophies({cmdr}) {
-    let tGold,tSilver,tBronze = 0;
+    let tGold = 0;
+    let tSilver = 0;
+    let tBronze = 0;
     let scr = 0;
     console.log("----------");
     console.log({cmdr});
 
     cmdr.forEach(indx =>
-            indx.lessons.forEach(indy =>
-                console.log(indy.name + " - " + indy.score)
+            indx.lessons.forEach(indy =>{
+                //console.log(indy.name + " - " + indy.score)
+                if(indy.score > 0.9){
+                    tGold++;
+                }
+                if(indy.score > 0.5 && indy.score < 0.9){
+                    tSilver++;
+                }
+                if(indy.score > 0.1 && indy.score < 0.5){
+                    tBronze++;
+                }
+            }
                 )
         )
     
-    return ( <div> TROPHIES  </div>)
 
-/*
-    props.lesson.forEach(lesson => {
-        if(lesson.score > 0.9){
-            tGold++;
-        }
-        if(lesson.score > 0.5 && lesson.score < 0.9){
-            tSilver++;
-        }
-        if(lesson.score > 0.1 && lesson.score < 0.5){
-            tBronze++;
-        }
-        
-    })
     console.log(tGold + " " + tSilver + " " + tBronze);
 
-    return (<React.Fragment>
-    <p>{tGold}{tSilver}{tBronze}</p>
+    return (
+    <React.Fragment>
+    <p>Gull: {tGold} Sølv: {tSilver} Bronse: {tBronze}</p>
     </React.Fragment>);
-    */
 }
 
 
